7a5df9b27dee3868c2d7bface1fee03d9573ef22
ElenaKonov <nega-le@yandex.ru>
2022-10-10 01:38:22 +0500

    add Task_2_4.py

--- Task_2_4.py
+++ Task_2_4.py
@@ -1,7 +1,18 @@
 #Реализуйте алгоритм перемешивания списка.
+import random
 
-list=[]
-n=int(input('Введите количество элементов в списке'))
-for i in (0,n+1):
+
+def get_list(n):
+ list=[]
+ for i in range(0,n+1):
     list.append(2*i+3)
+ return list
+
+
+n=int(input('Введите количество элементов в списке'))
+list=get_list(n)
 print(list)
+
+list1=list[:]
+random.shuffle(list1)
+print(list1)
