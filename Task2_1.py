82a156cc957bb29fedd8ed25fc2c3da6cf61ae2d
ElenaKonov <nega-le@yandex.ru>
2022-10-09 23:39:47 +0500

    add Task2_1.py

--- Task2_1.py
+++ Task2_1.py
@@ -5,11 +5,23 @@
 #- 6782 -> 23
 #- 0,56 -> 11
 
-num=float(input('ВВедите число'))
-sum=0
-while num > 0:
-    sum=sum+num % 10
-    num=num//10
-print(round(sum,0))
+num=float(input('ВВедите число: '))
 
+num1=int(num)
+num2=int(round(num%1,10)*pow(10,10))
+
+
+
+sum1=0
+while num1 > 0:
+    sum1=sum1+num1 % 10
+    num1=num1//10
+
+
+sum2=0
+while num2 > 0:
+    sum2=sum2+num2 % 10
+    num2=num2//10
+
+print('Сумма всех цифр, введенного числа, равна', sum1+sum2)
 
