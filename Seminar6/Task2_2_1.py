4b2e3cb69f288d2ed82abe5512a7c3d7a1491c0f
ElenaKonov <nega-le@yandex.ru>
2022-10-30 16:54:28 +0500

    add Task 2_2_1

--- Seminar6/Task2_2_1.py
+++ Seminar6/Task2_2_1.py
@@ -3,11 +3,12 @@
 #Пример:
 
 #- пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
-
+#num=int(input("ВВедите число"))
+#n=1
+#for i in range(1, (num+1)):
+    #n=n*i
+    #print(n, end= ' ')
 import math
-
-
 num=int(input("ВВедите число: "))
-
 list=[math.factorial(n) for n in range(1, (num+1))  ]
 print(list)
--- Seminar6/Task2_2_2.py
+++ Seminar6/Task2_2_2.py
@@ -1,3 +1,12 @@
+
+
+##num=int(input("ВВедите число"))
+#n=1
+#for i in range(1, (num+1)):
+    #n=n*i
+    #print(n, end= ' ')
+
+
 num=int(input("ВВедите число: "))
 def fact(n): 
     if(n==1 or n==0):
