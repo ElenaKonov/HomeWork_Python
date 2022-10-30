796f3e9cc00c7669cf66b73ce7ab0d2dd26dbd3c
ElenaKonov <nega-le@yandex.ru>
2022-10-30 16:59:09 +0500

    add Task3_1_1

--- Seminar6/Task3_1_1.py
+++ Seminar6/Task3_1_1.py
@@ -4,6 +4,29 @@
 #Пример:
 
 #- [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+
+
+#def get_list(n):
+  #import random
+  #list=[]
+  #for i in range(0,n):
+   # list.append(random.randint(-20,20))
+  #return list
+
+#def find_sum(list):
+    #sum=0
+    #for i in range(0,n):
+       # if (i%2==1):
+         #sum=sum+list[i]
+    #return sum        
+
+#n=int(input('Введите количество элементов в списке: '))
+#list=get_list(n)
+#print(list)
+#sum=find_sum(list)
+#rint(sum)
+
 import random
 n=int(input('Введите количество элементов в списке: ')) 
 
